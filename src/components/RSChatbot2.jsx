376f01c63935820906a617cf1d66882c534421dd
richis11 <thesaintsking@gmail.com>
2023-11-28 05:48:33 -0500

    ligero cambio rschatbot

--- src/components/RSChatbot2.jsx
+++ src/components/RSChatbot2.jsx
@@ -19,7 +19,7 @@ const ResponseComponent = ({ steps, triggerNextStep }) => {
       const res = await fetch('http://localhost:3001/api/generate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ texto: userInput, user: user.username }),
+        body: JSON.stringify({ texto: userInput, user: user.username, userid: user.userid}),
       });
       const data = await res.json();
       setResponse(data.result);
