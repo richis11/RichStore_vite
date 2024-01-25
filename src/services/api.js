46cf81a6185a1227e3b88422139306fc8e7f5407
richis11 <thesaintsking@gmailcom>
2024-01-25 00:24:32 -0500

    buscar productos, correcciones de login etc

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:3000/api"
-   // baseURL:"https://richstorebackend.up.railway.app/api/"
+   // baseURL:"http://localhost:3000/api"
+    baseURL:"https://richstorebackend.up.railway.app/api/"
 })
 
 export default api;
