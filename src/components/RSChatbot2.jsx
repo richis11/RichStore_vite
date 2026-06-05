d311184b70f7441934d6d8f744bde48f3be3063d
richis11 <thesaintsking@gmailcom>
2026-06-05 17:38:17 -0500

    creado archivo vercel.json

--- src/components/RSChatbot2.jsx
+++ src/components/RSChatbot2.jsx
@@ -80,10 +80,10 @@ const ResponseComponent = ({ steps, triggerNextStep }) => {
         })
       );
       
-      // const res = await fetch("http://localhost:3001/api/generate", {
+       const res = await fetch("http://localhost:5000/api/generate", {
       //const res = await fetch('https://richard11.pythonanywhere.com/api/generate', {
       //const res = await fetch("https://richis11dev.pythonanywhere.com/api/generate", {
-      const res = await fetch("https://rstorechatbot-production.up.railway.app/api/generate", {
+      //const res = await fetch("https://rstorechatbot-production.up.railway.app/api/generate", {
 
       
         method: "POST",
